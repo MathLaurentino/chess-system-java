2839c99578d0a3de73b99623c86efedc02495eaf
Laurentino <matheus.laurentino.ifpr@gmail.com>
2023-10-15 20:58:22 -0300

    Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -21,6 +21,11 @@ public class Board {
         return pieces[position.getRow()][position.getColumn()];
     }
 
+    public void placePiece(Piece piece, Position position) {
+        piece.position = position;
+        pieces[position.getRow()][position.getColumn()] = piece;
+    }
+
 
     public int getRows() {
         return rows;
