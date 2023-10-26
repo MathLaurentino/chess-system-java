67ce06bae95f15c7d719a27da8e5187c91d6e9b4
Laurentino <matheus.laurentino.ifpr@gmail.com>
2023-10-26 16:04:32 -0300

    Handling exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -31,6 +31,12 @@ public class UI {
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
 
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    } 
+
+
     /**
      * Reads the user input for a chess position.
      */
